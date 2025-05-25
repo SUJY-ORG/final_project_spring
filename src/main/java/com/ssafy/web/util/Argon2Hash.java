a96cd868f3458e64a530da67bc7ffbedccb27b0a
suee97 <atn1su@gmail.com>
2025-05-25 23:56:49 +0900

    feat: argon2 암호화 클래스 구현

---
+++ src/main/java/com/ssafy/web/util/Argon2Hash.java
@@ -0,0 +1,42 @@
+package com.ssafy.web.util;
+
+import org.bouncycastle.crypto.generators.Argon2BytesGenerator;
+import org.bouncycastle.crypto.params.Argon2Parameters;
+
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class Argon2Hash {
+
+    private static final int SALT_SIZE = 16;
+    private static final int ITERATION = 20;
+    private static final int MEM_LIMIT = 66536;
+    private static final int HASH_LENGTH = 32;
+    private static final int PARALLELISM = 1;
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public static byte[] createSalt() {
+        byte[] salt = new byte[SALT_SIZE];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public static String createHash(byte[] salt, String password) {
+        Argon2Parameters.Builder builder = new Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
+                .withVersion(Argon2Parameters.ARGON2_VERSION_13)
+                .withIterations(ITERATION)
+                .withMemoryAsKB(MEM_LIMIT)
+                .withParallelism(PARALLELISM)
+                .withSalt(salt);
+        Argon2BytesGenerator generator = new Argon2BytesGenerator();
+        generator.init(builder.build());
+        byte[] result = new byte[HASH_LENGTH];
+        generator.generateBytes(password.getBytes(StandardCharsets.UTF_8), result, 0, result.length);
+        return Base64.getEncoder().encodeToString(result);
+    }
+
+    private Argon2Hash() {}
+
+}
