a1a6fdd7c84637f66e2812c2b6a7c2603b975919
cheatz <cheatz@naver.com>
2025-05-26 16:22:48 +0900

    refactor: db설계 sql 수정

--- src/main/resources/init_database.sql
+++ src/main/resources/init_database.sql
@@ -1,11 +1,11 @@
 USE ssafyhome;
 DROP TABLE IF EXISTS registered_sales;
+DROP TABLE IF EXISTS ai_judge_sales;
 DROP TABLE IF EXISTS users;
-
 
 CREATE TABLE users (
 	user_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    service_id VARCHAR(50) NOT NULL,
+    service_id VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255),
     preference VARCHAR(255)
@@ -18,6 +18,14 @@ CREATE TABLE registered_sales(
     price BIGINT NOT NULL,
     lat DOUBLE NOT NULL,
     lng DOUBLE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE ai_judge_sales (
+	AI_judge_sales_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT,
+    sales_id BIGINT,
+    judge_cnt BIGINT DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
